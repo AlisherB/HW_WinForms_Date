4a86535e8b770bd336e2c5f9097b01be1ee15e13
AlisherB <alisher_b_95@mail.ru>
2017-09-22 14:09:57 +0600

    HW_WinForms_Date
    Домашнее задание(Работа с датой)

---
+++ HW_Date/Task/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.textBoxRes = new System.Windows.Forms.TextBox();
+            this.textBoxEnter = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonEnter.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonEnter.Location = new System.Drawing.Point(59, 53);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(110, 35);
+            this.buttonEnter.TabIndex = 0;
+            this.buttonEnter.Text = "OK";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClear.Location = new System.Drawing.Point(59, 154);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(110, 35);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // textBoxRes
+            // 
+            this.textBoxRes.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.textBoxRes.Location = new System.Drawing.Point(13, 112);
+            this.textBoxRes.Name = "textBoxRes";
+            this.textBoxRes.Size = new System.Drawing.Size(199, 20);
+            this.textBoxRes.TabIndex = 3;
+            this.textBoxRes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBoxEnter
+            // 
+            this.textBoxEnter.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.textBoxEnter.Location = new System.Drawing.Point(13, 13);
+            this.textBoxEnter.Name = "textBoxEnter";
+            this.textBoxEnter.Size = new System.Drawing.Size(199, 20);
+            this.textBoxEnter.TabIndex = 4;
+            this.textBoxEnter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 261);
+            this.Controls.Add(this.textBoxEnter);
+            this.Controls.Add(this.textBoxRes);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonEnter);
+            this.MaximumSize = new System.Drawing.Size(245, 300);
+            this.MinimumSize = new System.Drawing.Size(245, 300);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.TextBox textBoxRes;
+        private System.Windows.Forms.TextBox textBoxEnter;
+    }
+}
+
---
+++ HW_Date/Task/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+            DateTime dateTime = new DateTime();
+            try
+            {
+                dateTime = DateTime.Parse(textBoxEnter.Text);
+                textBoxRes.Text = dateTime.ToString("dddd");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Дата введена неверно (дд.мм.гггг)", "Error");
+                textBoxEnter.Clear();
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxRes.Clear();
+        }
+    }
+}
